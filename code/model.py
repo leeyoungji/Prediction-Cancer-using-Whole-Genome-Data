dc13f13601a5f36ed59bef517e732946403c0dd1
leeyoungji <61442376+leeyoungji@users.noreply.github.com>
2021-12-24 14:20:35 +0900

    Update model.py

--- code/model.py
+++ code/model.py
@@ -1,18 +1,12 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-#44900
-SEQ_LEN = 44900 # maximum length of an input sequence
+SEQ_LEN = 44900
 BATCH_SIZE = 8
 EMBED_SIZE = 16
 KERNEL_SIZE = 3
 STRIDE = 1
-PADDING = 1 # KERNEL_SIZE - 2
-# LEARNING_RATE = 0.01
-# WEIGHT_DECAY = 1e-4
-VERBOSE = False
-SAVE_EVERY = 10
+PADDING = 1
 
 PAD = "<PAD>" # padding
 PAD_IDX = 0
@@ -24,7 +18,6 @@ class CharCNN(nn.Module):
     def __init__(self, args):
         super(CharCNN, self).__init__()
         self.k = 64
-        #64
         self.conv1 = nn.Conv1d(args.num_features,128, kernel_size=3, stride=1, padding=1)
 
         self.res_blocks = nn.Sequential(  # residual blocks
@@ -42,23 +35,18 @@ class CharCNN(nn.Module):
             nn.ReLU(),
             nn.Linear(1024, 12)
         )
-
-        self.softmax = nn.Softmax()
-
-
+        self.softmax = nn.LogSoftmax()
 
     def forward(self, x):
-        # x = self.embed(x)  # embedding
-        # x = x.transpose(1, 2)  # [batch_size (N), num_feature_maps (D), seq_len (L)]
+
         x = self.conv1(x)
-        # h = self.conv(x)  # temporal convolution
         h = self.res_blocks(x)  # residual blocks
         h = h.topk(self.k)[0].view(BATCH_SIZE, -1)  # k-max pooling
         h = self.fc(h)  # fully connected layers
         y = self.softmax(h)
         return y
 
-class res_block(nn.Module):  # residual block
+class res_block(nn.Module):
     def __init__(self, in_channels, out_channels, downsample=None):
         super().__init__()
         first_stride = 2 if downsample == "resnet" else 1
@@ -67,7 +55,7 @@ class res_block(nn.Module):  # residual block
         # architecture
         self.conv_block = conv_block(in_channels, out_channels, first_stride)
         self.pool = None
-        if downsample == "kmax":  # k-max pooling (Kalchbrenner et al 2014)
+        if downsample == "kmax":  # k-max pooling
             self.pool = lambda x: x.topk(x.size(2) // 2)[0]
         elif downsample == "vgg":  # VGG-like
             self.pool = nn.MaxPool1d(KERNEL_SIZE, pool_stride, PADDING)
@@ -80,7 +68,7 @@ class res_block(nn.Module):  # residual block
         y += self.shortcut(x)  # ResNet shortcut connections
         return y
 
-class conv_block(nn.Module):  # convolutional block
+class conv_block(nn.Module):
     def __init__(self, in_channels, out_channels, first_stride):
         super().__init__()
 
